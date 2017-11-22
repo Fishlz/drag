ab9aae5b7856d5dc2be5ff8a45afb576b1622ec7
Fishlz <liuzhengTUST@163.com>
2017-11-22 21:18:19 +0800

    success drag

--- drag.js
+++ drag.js
@@ -2,12 +2,19 @@ function drag(id){
     var obj = document.getElementById(id);
     var disX=0;
     var disY=0;
-    obj.onmousedown =function(e){
-        disX=e.pageX-obj.offsetLeft;
-        disY=e.pageY-obj.offsetTop;
-        document.onmousemove=function(e){
-            obj.style.left=e.pageX-disX+'px';
-            obj.style.top=e.pageY-disY+'px';
+    obj.onmousedown =function(ev){
+        disX=ev.pageX-obj.offsetLeft;
+        disY=ev.pageY-obj.offsetTop;
+        document.onmousemove=function(ev){
+            obj.style.left=ev.pageX-disX+'px';
+            obj.style.top=ev.pageY-disY+'px';
+        };
+        document.onmouseup=function(){
+            document.onmousemove=null;
+            document.onmouseup=null;
         };
     };
+    return false;
 }
+
+drag('div1');
